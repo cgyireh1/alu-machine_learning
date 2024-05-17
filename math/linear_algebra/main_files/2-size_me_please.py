387f3d151c97ed6d072ff14c23e245c8265ddbd2
cgyireh1 <c.gyireh@alustudent.com>
2024-05-16 22:35:00 -0700

    transpose

---
+++ math/linear_algebra/main_files/2-size_me_please.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
