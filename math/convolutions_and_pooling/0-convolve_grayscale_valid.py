f235bd6726e193a5154bcf0c5b21ec983a6cce43
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-07-06 05:12:00 +0200

    Update 0-convolve_grayscale_valid.py

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -12,7 +12,7 @@ def convolve_grayscale_valid(images, kernel):
     -m is the number of images
     -h is the height in pixels of the images
     -w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw) 
+    kernel is a numpy.ndarray with shape (kh, kw)
     -kh is the height of the kernel
     -kw is the width of the kernel
     """
@@ -28,7 +28,7 @@ def convolve_grayscale_valid(images, kernel):
 
     image = np.arange(m)
     for x in range(output_h):
-      for y in range(output_w):
+        for y in range(output_w):
             conv_out[image, x, y] = (np.sum(images[image, x:kh+x,
-                                        y:kw+y] * kernel))
+                                            y:kw+y] * kernel))
     return conv_out
