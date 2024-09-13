cae640fc37d69781e794af22de6bd400d2554b49
cgyireh1 <c.gyireh@alustudent.com>
2024-09-13 16:39:18 -0700

    tasks on neural networks

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
-"""Neural Network"""
+""" Neural Network """
 
 import numpy as np
 
 
 class NeuralNetwork():
-  """
-  A class that defines a neural network
-  with one hidden layer performing binary classification
-  """
+    """ 
+    A class that defines a neural network
+    with one hidden layer performing binary classification
+    """
 
-  def __init__(self, nx, nodes):
-    if type(nx) is not int:
-      raise TypeError("nx must be an integer")
-    if nx < 1:
-      raise ValueError("nx must be a positive integer")
-    if type(nodes) is not int:
-      raise TypeError("nodes must be an integer")
-    if nodes < 1:
-      raise ValueError("nodes must be a positive integer")
+    def __init__(self, nx, nodes):
 
-    self.__W1 = np.random.randn(nodes, nx)
-    self.__b1 = np.zeros((nodes, 1))
-    self.__A1 = 0
-    self.__W2 = np.random.randn(1, nodes)
-    self.__b2 = 0
-    self.__A2 = 0
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
