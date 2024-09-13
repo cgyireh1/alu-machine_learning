ea3adab496009c4884b030c8376b93e5caa44951
cgyireh1 <c.gyireh@alustudent.com>
2024-09-13 06:34:03 -0700

    classification

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -6,16 +6,13 @@ Neuron class
 import numpy as np
 
 def __init__(self, nx):
-
-  """
-  A class Neuron that defines a single 
-  neuron performing binary classification
-  """
-
-  if type(nx) is not int:
+ """
+ A class Neuron that defines a single neuron performing binary classification
+ """
+ if type(nx) is not int:
     raise TypeError("nx must be an integer")
-  if nx < 1:
+ if nx < 1:
     raise ValueError("nx must be a positive integer")
-  self.__W = np.random.randn(1, nx)
-  self.__b = 0
-  self.__A = 0
+ self.__W = np.random.randn(1, nx)
+ self.__b = 0
+ self.__A = 0
--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -5,15 +5,13 @@ import numpy as np
 
 def __init__(self, nx):
   """
-  A class Neuron that defines a single neuron 
+  A class Neuron that defines a single neuron
   performing binary classification(Based on 0-neuron.py)
   """
-
-  def __init__(self, nx):
-    if type(nx) is not int:
-      raise TypeError("nx must be an integer")
-    if nx < 1:
-      raise ValueError("nx must be a positive integer")
-    self.__W = np.random.randn(1, nx)
-    self.__b = 0
-    self.__A = 0
+  if type(nx) is not int:
+     raise TypeError("nx must be an integer")
+  if nx < 1:
+     raise ValueError("nx must be a positive integer")
+  self.__W = np.random.randn(1, nx)
+  self.__b = 0
+  self.__A = 0
