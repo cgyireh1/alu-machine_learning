66ffb696788cb067d6142481a644995e9439fded
cgyireh1 <c.gyireh@alustudent.com>
2024-09-13 07:28:13 -0700

    classification

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -22,12 +22,12 @@ class Neuron():
 
     @property
     def W(self):
-      return self.__W
+        return self.__W
     
     @property
     def b(self):
-      return self.__b
+        return self.__b
 
     @property
     def A(self):
-      return self.__A
+        return self.__A
