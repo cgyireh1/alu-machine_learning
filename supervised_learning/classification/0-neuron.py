5e98069fb94a8ffbcb2d9ed29547319aab26b385
Caro <carolinegyireh@gmail.com>
2024-09-13 13:38:35 +0200

    classification

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Neuron class
+"""
+
+import numpy as np
+
+def __init__(self, nx):
+
+  """
+  A class Neuron that defines a single 
+  neuron performing binary classification
+  """
+
+  if type(nx) is not int:
+    raise TypeError("nx must be an integer")
+  if nx < 1:
+    raise ValueError("nx must be a positive integer")
+  self.__W = np.random.randn(1, nx)
+  self.__b = 0
+  self.__A = 0
---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Privatize Neuron"""
+
+import numpy as np
+
+def __init__(self, nx):
+  """
+  A class Neuron that defines a single neuron 
+  performing binary classification(Based on 0-neuron.py)
+  """
+
+  def __init__(self, nx):
+    if type(nx) is not int:
+      raise TypeError("nx must be an integer")
+    if nx < 1:
+      raise ValueError("nx must be a positive integer")
+    self.__W = np.random.randn(1, nx)
+    self.__b = 0
+    self.__A = 0
