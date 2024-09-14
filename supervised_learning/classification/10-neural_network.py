39686d78aa95d0373729ff4d38b0c689c490b384
cgyireh1 <c.gyireh@alustudent.com>
2024-09-13 17:01:23 -0700

    tasks on neural networks

--- supervised_learning/classification/10-neural_network.py
+++ supervised_learning/classification/10-neural_network.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
-""" Neural Network """
+"""Neural Network"""
 
 import numpy as np
 
 
 class NeuralNetwork():
-    """
-    A class that defines a neural network
-    with one hidden layer performing binary classification
-    """
+    """ A class that defines a neural network
+    with one hidden layer performing binary classification"""
 
     def __init__(self, nx, nodes):
-
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -28,35 +25,33 @@ class NeuralNetwork():
         self.b2 = 0
         self.A2 = 0
 
+    @property
+    def W1(self):
+        return self.__W1
 
-        @property
-        def W1(self):
-          return self.__W1
-        
-        @property
-        def b1(self):
-          return self.__b1
+    @property
+    def b1(self):
+        return self.__b1
 
-        @property
-        def A1(self):
-          return self.__A1
-        
-        @property
-        def W2(self):
-          return self.__W2
-        
-        @property
-        def b2(self):
-          return self.__b2
+    @property
+    def A1(self):
+        return self.__A1
 
-        @property
-        def A2(self):
-          return self.__A2
-    
+    @property
+    def W2(self):
+        return self.__W2
 
-        def forward_prop(self, X):
-          """Calculates the forward propagation of the neural network"""
+    @property
+    def b2(self):
+        return self.__b2
 
-          self.__A1 = 1 / (1 + np.exp(-np.matmul(self.__W1, X) + self.__b1))
-          self.__A2 = 1 / (1 + np.exp(-np.matmul(self.__W2, self.__A1) + self.__b2))
-          return self.__A1, self.__A2
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+        self.__A1 = 1 / (1 + np.exp(-np.matmul(self.__W1, X) + self.__b1))
+        self.__A2 = 1 / (1 + np.exp(-np.matmul(self.__W2, self.__A1) + self.__b2))
+
+        return self.__A1, self.__A2
