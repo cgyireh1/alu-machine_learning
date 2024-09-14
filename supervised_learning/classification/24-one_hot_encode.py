33b5033661f708768cb7e772fe736f6f73ec0e44
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-14 10:40:12 +0200

    Update

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -11,9 +11,9 @@ def one_hot_encode(Y, classes):
     """
     A function that converts a numeric label vector into a one-hot matrix
     """
-    if type(Y) is not np.ndarray or len(Y.shape) != 1 or len(Y) < 1:
+    if type(Y) is not np.ndarray or len(Y.shape) != 1:
         return None
-    if type(classes) is not int or classes != (Y.max()):
+    if type(classes) is not int or classes != (Y.max() + 1):
         return None
     one_hot_enc = np.eye(classes)[Y].transpose()
     return one_hot_enc
