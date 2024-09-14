794152167380832b387ddf008da2659382e466d9
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-14 10:12:17 +0200

    Create

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+One-Hot Encode
+"""
+
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """
+     A function that converts a numeric label vector into a one-hot matrix
+    """
+    if type(Y) is not np.ndarray:
+        return None
+    if type(classes) is not int:
+        return None
+
+    one_hot = np.eye(classes)[Y].transpose()
+    return one_hot
