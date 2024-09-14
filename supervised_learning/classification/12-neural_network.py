2cbb71d8303b7b7db016a03242969a1da50b4b48
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-14 08:08:04 +0200

    Update 12-neural_network.py

--- supervised_learning/classification/12-neural_network.py
+++ supervised_learning/classification/12-neural_network.py
@@ -18,12 +18,12 @@ class NeuralNetwork():
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
 
     @property
     def W1(self):
@@ -50,7 +50,7 @@ class NeuralNetwork():
         return self.__A2
 
     def forward_prop(self, X):
-        """Calculates the forward propagation of the neural network"""
+        """ Calculates the forward propagation of the neural network """
         self.__A1 = 1 / (1 + np.exp(-np.matmul(self.__W1, X) + self.__b1))
         self.__A2 = 1 / (1 + np.exp(-np.matmul(self.__W2, self.__A1)
                          + self.__b2))
@@ -58,11 +58,9 @@ class NeuralNetwork():
         return self.__A1, self.__A2
 
     def cost(self, Y, A):
-        """Calculates the cost of the model using logistic regression"""
-
+        """ Calculates the cost of the model using logistic regression """
         Cost = - (1 / Y.shape[1]) * np.sum(
-            np.multiply(
-                Y, np.log(A)) + np.multiply(
+            np.multiply(Y, np.log(A)) + np.multiply(
                 1 - Y, np.log(1.0000001 - A)))
         return Cost
 
@@ -70,7 +68,6 @@ def evaluate(self, X, Y):
         """
         Evaluates the neural network’s predictions
         """
-
         self.forward_prop(X)
         cost = self.cost(Y, self.__A2)
         prediction = np.where(self.__A2 >= 0.5, 1, 0)
