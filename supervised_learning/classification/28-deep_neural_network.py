95b4fa592f26eff78c3fd3248d3b8ed063611c50
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-14 19:14:18 +0200

    Update

--- supervised_learning/classification/28-deep_neural_network.py
+++ supervised_learning/classification/28-deep_neural_network.py
@@ -19,10 +19,13 @@ class DeepNeuralNetwork():
             raise TypeError("layers must be a list of positive integers")
         if len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
+        if activation != 'sig' and activation != 'tanh':
+            raise ValueError("activation must be 'sig' or 'tanh'")
 
         self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
+        self.__activation = activation
         for k in range(self.L):
             if layers[k] < 1 or type(layers[k]) is not int:
                 raise TypeError("layers must be a list of positive integers")
