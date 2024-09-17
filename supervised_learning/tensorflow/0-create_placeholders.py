ea498a95b207cca934d554c1066f4e4dcc98260f
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-17 08:01:52 +0200

    Update 0-create_placeholders

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" 
+"""
 A function that returns two placeholders, x and y,
 for the neural network
 """
@@ -8,14 +8,14 @@ import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
-  """"
-  nx: the number of feature columns in our data
-  classes: the number of classes in our classifier
-        Returns: placeholders named x and y, respectively
-  x is the placeholder for the input data to the neural network
-  y is the placeholder for the one-hot labels for the input data
-  """
-
-  x = tf.placeholder(tf.float32, shape=(None, nx), name="input_data")
-  y = tf.placeholder(tf.float32, shape=(None, classes), name="one-hot_labels")
-  return x, y
+    """"
+    nx: the number of feature columns in our data
+    classes: the number of classes in our classifier
+          Returns: placeholders named x and y, respectively
+    x is the placeholder for the input data to the neural network
+    y is the placeholder for the one-hot labels for the input data
+    """
+  
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
+    return x, y
