169975079261280019dcc10f54ba49708cfdb375
Caro <carolinegyireh@gmail.com>
2024-09-19 06:45:11 +0200

    layers

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -8,14 +8,14 @@ import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
-  """"
-  nx: the number of feature columns in our data
-  classes: the number of classes in our classifier
-        Returns: placeholders named x and y, respectively
-  x is the placeholder for the input data to the neural network
-  y is the placeholder for the one-hot labels for the input data
-  """
+    """"
+    nx: the number of feature columns in our data
+    classes: the number of classes in our classifier
+    Returns: placeholders named x and y, respectively
+    x is the placeholder for the input data to the neural network
+    y is the placeholder for the one-hot labels for the input data
+    """
 
-  x = tf.placeholder(tf.float32, shape=(None, nx), name="input_data")
-  y = tf.placeholder(tf.float32, shape=(None, classes), name="one-hot_labels")
-  return x, y
+    x = tf.placeholder(tf.float32, shape=(None, nx), name="input_data")
+    y = tf.placeholder(tf.float32, shape=(None, classes), name="one-hot_labels")
+    return x, y
