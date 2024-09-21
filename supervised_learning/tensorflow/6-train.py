f37d192dc3752abd3057f3fe817c7e036d00036c
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-21 16:00:07 +0200

    Update 6-train

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Train """
 
+import tensorflow as tf
 
 calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
 calculate_loss = __import__('4-calculate_loss').calculate_loss
