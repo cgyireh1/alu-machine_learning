44949d58ba121fd68db7600fcd03fc7d1e29dda4
Caro <carolinegyireh@gmail.com>
2024-09-21 16:27:37 +0200

    evaluating neural network ouput

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -16,4 +16,19 @@ def evaluate(X, Y, save_path):
     Returns: the network’s prediction, accuracy, and loss
     """
 
-    
+    with tf.Session() as session:
+        saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+        saver.restore(session, save_path)
+
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("loss")[0]
+
+        feed_dict={x: X, y: Y}
+        evaluate_y_pred = session.run(y_pred, feed_dict=feed_dict)
+        evaluate_accuracy = session.run(accuracy, feed_dict=feed_dict)
+        evaluate_loss = session.run(loss, feed_dict=feed_dict)
+
+        return evaluate_y_pred, evaluate_accuracy, evaluate_loss
