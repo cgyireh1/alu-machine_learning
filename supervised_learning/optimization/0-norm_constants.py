7ecd94f2c410ec2fa51eb41ceaaee847eb434785
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:14:16 +0200

    Update 0-norm_constants

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -5,12 +5,12 @@ import numpy as np # type: ignore
 
 
 def normalization_constants(X):
-  """
-  X is the numpy.ndarray of shape (m, nx) to normalize
-  m is the number of data points
-  nx is the number of features
-  """
-
-  mean = np.mean(X, axis=0)
-  stdev = np.std(X, axis=0)
-  return mean, stdev
+    """
+    X is the numpy.ndarray of shape (m, nx) to normalize
+    m is the number of data points
+    nx is the number of features
+    """
+  
+    mean = np.mean(X, axis=0)
+    stdev = np.std(X, axis=0)
+    return mean, stdev
