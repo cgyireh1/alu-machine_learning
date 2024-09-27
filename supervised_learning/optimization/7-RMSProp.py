80336d5c08a6b498c9c96538788a29deedf7d337
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:11:48 +0200

    Update 7-RMSProp

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -5,16 +5,16 @@ import numpy as np # type: ignore
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
-  """
-  Alpha is the learning rate
-  beta2 is the RMSProp weight
-  epsilon is a small number to avoid division by zero
-  var is a numpy.ndarray containing the variable to be updated
-  grad is a numpy.ndarray containing the gradient of var
-  s is the previous second moment of var
-  Returns: the updated variable and the new moment, respectively
-  """
-
-  Sdv = (beta2 * s) + ((1 - beta2) * np.square(grad))
-  new_Var = var - alpha * (grad / np.sqrt(Sdv) + epsilon)
-  return new_Var, Sdv
+    """
+    Alpha is the learning rate
+    beta2 is the RMSProp weight
+    epsilon is a small number to avoid division by zero
+    var is a numpy.ndarray containing the variable to be updated
+    grad is a numpy.ndarray containing the gradient of var
+    s is the previous second moment of var
+    Returns: the updated variable and the new moment, respectively
+    """
+  
+    Sdv = (beta2 * s) + ((1 - beta2) * np.square(grad))
+    new_Var = var - alpha * (grad / np.sqrt(Sdv) + epsilon)
+    return new_Var, Sdv
