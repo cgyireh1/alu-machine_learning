7e04bdec42567c903653d806006f7616ebd8dbcf
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:25:34 +0200

    Update 3-l2_reg_create_layer

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -5,17 +5,17 @@ import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-  """
-  prev is a tensor containing output of the previous layer
-  n is the number of nodes the new layer should contain
-  activation is the activation function that
-  should be used on the layer
-  lambtha is the L2 regularization parameter
-  """
+    """
+    prev is a tensor containing output of the previous layer
+    n is the number of nodes the new layer should contain
+    activation is the activation function that
+    should be used on the layer
+    lambtha is the L2 regularization parameter
+    """
 
-  regularizer = tf.contrib.layers.l2_regularizer(lambtha)
-  initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-  tensor = tf.layers.Dense(units=n, activation=activation,
-                           kernel_initializer=initializer,
-                           kernel_regularizer=regularizer)
-  return tensor(prev)
+    regularizer = tf.contrib.layers.l2_regularizer(lambtha)
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    tensor = tf.layers.Dense(units=n, activation=activation,
+                             kernel_initializer=initializer,
+                             kernel_regularizer=regularizer)
+    return tensor(prev)
