028bdd81e11b14215ccaf00d54f41fcb04870cfc
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:28:15 +0200

    Update 6-dropout_create_layer

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -5,18 +5,19 @@ import tensorflow as tf
 
 
 def dropout_create_layer(prev, n, activation, keep_prob):
-  """
-  prev is a tensor containing the output of the previous layer
-  n is the number of nodes the new layer should contain
-  activation is activation function that should be used on the layer
-  keep_prob is the probability that a node will be kept
-  """
+    """
+    prev is a tensor containing the output of the previous layer
+    n is the number of nodes the new layer should contain
+    activation is activation function that should be used on the layer
+    keep_prob is the probability that a node will be kept
+    """
 
-  dropout_layer = tf.keras.layers.Dropout(keep_prob)
-  initiator_layer = tf.keras.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    dropout_layer = tf.keras.layers.Dropout(keep_prob)
+    initiator_layer = tf.keras.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"
+                                                                          )
 
-  dense_layer = tf.keras.layers.Dense(
-    units=n, activation=activation,
-    kernel_initializer=initiator_layer,
-    kernel_regularizer=dropout_layer)(prev)
-  return dense_layer
+    dense_layer = tf.keras.layers.Dense(
+      units=n, activation=activation,
+      kernel_initializer=initiator_layer,
+      kernel_regularizer=dropout_layer)(prev)
+    return dense_layer
