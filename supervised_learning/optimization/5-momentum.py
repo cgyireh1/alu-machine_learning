29e09b0ed3183bce81b2f0425321a69dac26e888
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:13:12 +0200

    Update 5-momentum

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -3,15 +3,15 @@
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
-  """
-  alpha is the learning rate
-  beta1 is the momentum weight
-  var is a numpy.ndarray containing the variable to be updated
-  grad is a numpy.ndarray containing the gradient of var
-  v is the previous first moment of var
-  Returns: the updated variable and the new moment, respectively
-  """
-
-  V = (beta1 * v) + ((1 - beta1) * grad)
-  Var = var - (alpha * V)
-  return Var, V
+    """
+    alpha is the learning rate
+    beta1 is the momentum weight
+    var is a numpy.ndarray containing the variable to be updated
+    grad is a numpy.ndarray containing the gradient of var
+    v is the previous first moment of var
+    Returns: the updated variable and the new moment, respectively
+    """
+  
+    V = (beta1 * v) + ((1 - beta1) * grad)
+    Var = var - (alpha * V)
+    return Var, V
