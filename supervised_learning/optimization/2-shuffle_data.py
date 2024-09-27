27d05d4a1cfde008feb136d80060630ef2eeeeee
Caro <carolinegyireh@gmail.com>
2024-09-27 21:05:13 +0200

    updates

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -14,8 +14,8 @@ def shuffle_data(X, Y):
     ny is the number of features in Y
     Returns: the shuffled X and Y matrices
     """
-
-    X_shuffled = X[np.random.permutation(X.shape[0])]
-    Y_shuffled = Y[np.random.permutation(X.shape[0])]
+    perm = np.random.permutation(X.shape[0])
+    X_shuffled = X[perm]
+    Y_shuffled = Y[perm]
 
     return X_shuffled, Y_shuffled
