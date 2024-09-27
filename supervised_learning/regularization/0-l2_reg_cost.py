6a614df4b38a459ead2f5e56f2ed5c75080922a7
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:23:44 +0200

    Update 0-l2_reg_cost

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """L2 Regularization Cost"""
 
-import numpy as np # type: ignore
+import numpy as np
 
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
-  """
-  cost is the cost of the network without L2 reg
-  lambtha is the regularization parameter
-  weights is a dictionary of the weights
-  and biases of the neural network
-  L is the number of layers in the neural network
-  m is the number of data points used
-  """
+    """
+    cost is the cost of the network without L2 reg
+    lambtha is the regularization parameter
+    weights is a dictionary of the weights
+    and biases of the neural network
+    L is the number of layers in the neural network
+    m is the number of data points used
+    """
 
-  l2_reg = 0
-  for key, values in weights.items():
-        if key[0] == 'W':
-            l2_reg += np.linalg.norm(values)
-  L2_cost = cost + (lambtha / (2 * m) * l2_reg)
-  return L2_cost
+    l2_reg = 0
+    for key, values in weights.items():
+          if key[0] == 'W':
+              l2_reg += np.linalg.norm(values)
+    L2_cost = cost + (lambtha / (2 * m) * l2_reg)
+    return L2_cost
