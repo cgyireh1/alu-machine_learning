ce94ba0cfedb2d3632274cb91cf380363953a695
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:13:31 +0200

    Update 2-shuffle_data

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -5,17 +5,17 @@ import numpy as np # type: ignore
 
 
 def shuffle_data(X, Y):
-  """
-  X is the first numpy.ndarray of shape (m, nx) to shuffle
-  m is the number of data points
-  nx is the number of features in X
-  Y is the second numpy.ndarray of shape (m, ny) to shuffle
-  m is the same number of data points as in X
-  ny is the number of features in Y
-  Returns: the shuffled X and Y matrices
-  """
-
-  X_shuffled = X[np.random.permutation(X.shape[0])]
-  Y_shuffled = Y[np.random.permutation(X.shape[0])]
-
-  return X_shuffled, Y_shuffled
+    """
+    X is the first numpy.ndarray of shape (m, nx) to shuffle
+    m is the number of data points
+    nx is the number of features in X
+    Y is the second numpy.ndarray of shape (m, ny) to shuffle
+    m is the same number of data points as in X
+    ny is the number of features in Y
+    Returns: the shuffled X and Y matrices
+    """
+  
+    X_shuffled = X[np.random.permutation(X.shape[0])]
+    Y_shuffled = Y[np.random.permutation(X.shape[0])]
+  
+    return X_shuffled, Y_shuffled
