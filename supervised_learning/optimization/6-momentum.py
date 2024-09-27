32a351b2fe7d2b9126a9e86c8f026627f4e25345
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:12:42 +0200

    Update 6-momentum

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 
 
 def create_momentum_op(loss, alpha, beta1):
-  """
-  loss is the loss of the network
-  alpha is the learning rate
-  beta1 is the momentum weight
-  Returns: the momentum optimization operation
-  """
-
-  optimizer = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
-  return optimizer
+    """
+    loss is the loss of the network
+    alpha is the learning rate
+    beta1 is the momentum weight
+    Returns: the momentum optimization operation
+    """
+  
+    optimizer = tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
+    return optimizer
