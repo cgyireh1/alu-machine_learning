83b94c4dd9f730458110ffc1a4237c8a2285dd2c
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-27 20:13:51 +0200

    Update 1-normalize

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -3,14 +3,14 @@
 
 
 def normalize(X, m, s):
-  """
-  X is the numpy.ndarray of shape (d, nx) to normalize
-  d is the number of data points
-  nx is the number of features
-  m is a numpy.ndarray of shape (nx,) that
-  contains the mean of all features of X
-  s is a numpy.ndarray of shape (nx,) that
-  contains the standard deviation of all features of X
-  """
-
-  return (X - m) / s
+    """
+    X is the numpy.ndarray of shape (d, nx) to normalize
+    d is the number of data points
+    nx is the number of features
+    m is a numpy.ndarray of shape (nx,) that
+    contains the mean of all features of X
+    s is a numpy.ndarray of shape (nx,) that
+    contains the standard deviation of all features of X
+    """
+  
+    return (X - m) / s
