88444122eb218457b02240d40eadf9ac6e8bd151
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-28 04:46:25 +0200

    Update 14-batch_norm

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -8,14 +8,19 @@ def create_batch_norm_layer(prev, n, activation):
     """
     prev is the activated output of the previous layer
     n is the number of nodes in the layer to be created
-    activation is the activation function that should be used on the output of the layer
-    you should use the tf.layers.Dense layer as the base layer with kernal initializer tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    your layer should incorporate two trainable parameters, gamma and beta, initialized as vectors of 1 and 0 respectively
+    activation is the activation function that should be used on the output
+    of the layer
+    you should use the tf.layers.Dense layer as the base layer with kernal
+    initializer tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    your layer should incorporate two trainable parameters, gamma and beta,
+    initialized as vectors of 1 and 0 respectively
     you should use an epsilon of 1e-8
     """
 
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    x = tf.layers.Dense(units=n, activation=None, kernel_initializer=initializer)
+    initializer = tf.contrib.layers.variance_scaling_initializer(mode=
+                                                                 "FAN_AVG")
+    x = tf.layers.Dense(units=n, activation=None, kernel_initializer=
+                        initializer)
     x_prev = x(prev)
     scale = tf.Variable(tf.constant(1.0, shape=[n]), name='gamma')
     mean, variance = tf.nn.moments(x_prev, axes=[0])
