32362bce62093ea90647165cb5cfb82b1dcc22bd
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-09-28 07:22:47 +0200

    Update 15-model

--- supervised_learning/optimization/15-model.py
+++ supervised_learning/optimization/15-model.py
@@ -30,7 +30,7 @@ def calculate_accuracy(y, y_pred):
     Calculate the accuracy of a prediction in a DNN
     """
     prediction = tf.equal(tf.argmax(y, 1),
-                                  tf.argmax(y_pred, 1))
+                          tf.argmax(y_pred, 1))
     accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 
     return accuracy
