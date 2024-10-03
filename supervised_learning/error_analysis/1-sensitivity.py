a13093f712d8a2e9da460f6bc955843d7c087af5
Caro <carolinegyireh@gmail.com>
2024-10-03 23:14:22 +0200

    error analysis

--- supervised_learning/error_analysis/1-sensitivity.py
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -5,17 +5,17 @@ import numpy as np
 
 
 def sensitivity(confusion):
-  """
-  confusion is a confusion numpy.ndarray of shape
-  (classes, classes) where row indices represent
-  the correct labels and column indices
-  represent the predicted labels
-  classes is the number of classes
-  Returns: a numpy.ndarray of shape (classes,)
-  containing the sensitivity of each class
-  """
+    """
+    confusion is a confusion numpy.ndarray of shape
+    (classes, classes) where row indices represent
+    the correct labels and column indices
+    represent the predicted labels
+    classes is the number of classes
+    Returns: a numpy.ndarray of shape (classes,)
+    containing the sensitivity of each class
+    """
 
-  TP = np.diag(confusion)
-  FN = np.sum(confusion, axis=1) - TP
-  Sensitivity = TP / (TP + FN)
-  return Sensitivity
+    TP = np.diag(confusion)
+    FN = np.sum(confusion, axis=1) - TP
+    Sensitivity = TP / (TP + FN)
+    return Sensitivity
--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" Precision """
+""" F1 score """
 
-import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
-def precision(confusion):
+def f1_score(confusion):
     """
     confusion is a confusion numpy.ndarray of shape
     (classes, classes) where row indices represent
@@ -12,10 +13,9 @@ def precision(confusion):
     represent the predicted labels
     classes is the number of classes
     Returns: a numpy.ndarray of shape (classes,)
-    containing the precision of each class
+    containing the F1 score of each class
     """
-
-    TP = np.diag(confusion)
-    FP = np.sum(confusion, axis=0) - TP
-    Precision = TP / (TP + FP)
-    return Precision
+    p = precision(confusion)
+    s = sensitivity(confusion)
+    f1 = 2 * (p * s) / (p + s)
+    return f1
