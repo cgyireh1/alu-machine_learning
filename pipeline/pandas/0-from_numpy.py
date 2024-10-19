fe9c4f71cbb9443acce2c903939bbc98ee6e0f40
Caro <carolinegyireh@gmail.com>
2024-10-19 11:41:59 +0200

    ml pipeline

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -8,8 +8,9 @@ def from_numpy(array):
     function  that creates a pd.DataFrame from a np.ndarray:
 
     array is the np.ndarray from which you should create the pd.DataFrame
-    The columns of the pd.DataFrame should be labeled in alphabetical order and capitalized. There will not be more than 26 columns.
-    Returns: the newly created pd.DataFrame
+    The columns of the pd.DataFrame should be labeled in alphabetical order
+    and capitalized. There will not be more than 26 columns.
+    Returns: the newly created pd.DataFram
     """
     c_list = list('ABCDEFGH')
     reshape = c_list[:array.shape[1]]
