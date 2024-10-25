a4249a6a1f33c22dfcac43cabd33bf1d03f81a73
Caro <carolinegyireh@gmail.com>
2024-10-25 17:41:00 +0200

    apis

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -5,8 +5,8 @@ import requests
 
 def sentientPlanets():
     """
-    By using the Swapi API, create a method that 
-    returns the list of names of the home 
+    By using the Swapi API, create a method that
+    returns the list of names of the home
     planets of all sentient species.
     Prototype: def sentientPlanets():
     Don’t forget the pagination
