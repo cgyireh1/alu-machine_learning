74833ab4ccfe4ae8eb205b0f26fde5344746a0b2
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-10-26 16:37:58 +0200

    Update 3-upcoming

--- pipeline/apis/3-upcoming.py
+++ pipeline/apis/3-upcoming.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Script that displays upcoming launch with these information:
+Script that displays the upcoming launch with these information:
 
 Name of the launch
 The date (in local time)
@@ -10,27 +10,36 @@ The name (with the locality) of the launchpad
 
 import requests
 
-if __name__ == '__main__':
+def get_upcoming_launch():
     url = 'https://api.spacexdata.com/v4/launches/upcoming'
-    response = requests.get(url).json()
-    date = [x['date_unix'] for x in response]
-    idx = date.index(min(date))
-    launch = response[idx]
+    response = requests.get(url)
+    response.raise_for_status()
+
+    upcoming_launch = min(response.json(), key=lambda x: x['date_unix'])
+
+    return upcoming_launch
+
+def get_rocket_name(rocket_id):
+    rocket_url = f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
+    response = requests.get(rocket_url)
+    response.raise_for_status()
+    return response.json()['name']
+
+def get_launchpad_info(launchpad_id):
+    launchpad_url = f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}"
+    response = requests.get(launchpad_url)
+    response.raise_for_status()
+    return response.json()
+
+if __name__ == '__main__':
+    launch = get_upcoming_launch()
     launch_name = launch['name']
-    date_l = launch['date_local']
-    rocket_id = launch['rocket']
-    rocket_url = "https://api.spacexdata.com/v4/rockets/{}".format(rocket_id)
-    rocket_name = requests.get(rocket_url).json()['name']
-    lpad_id = launch['launchpad']
-    lpad_url = "https://api.spacexdata.com/v4/launchpads/{}".\
-        format(lpad_id)
-    lpad_req = requests.get(lpad_url).json()
-    lpad_name = lpad_req['name']
-    lpad_loc = lpad_req['locality']
+    date_local = launch['date_local']
+    rocket_name = get_rocket_name(launch['rocket'])
+    launchpad_info = get_launchpad_info(launch['launchpad'])
+    launchpad_name = launchpad_info['name']
+    launchpad_locality = launchpad_info['locality']
 
-    upcoming_launch = "{} ({}) {} - {} ({})".format(launch_name, date_l,
-                                                    rocket_name, lpad_name,
-                                                    lpad_loc)
-
-    print(upcoming_launch)
+    upcoming_launch_info = f"{launch_name} ({date_local}) {rocket_name} - {launchpad_name} ({launchpad_locality})"
     
+    print(upcoming_launch_info)
