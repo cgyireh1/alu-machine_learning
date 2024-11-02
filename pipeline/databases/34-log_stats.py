7d2ff8700e6c632b1a1d8d9de194eb46fb1ef54c
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-11-02 20:25:24 +0200

    databases

---
+++ pipeline/databases/34-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+script that provides some stats about Nginx logs stored in MongoDB
+"""
+
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017')
+    logs_collection = client.logs.nginx
+    doc_count = logs_collection.count_documents({})
+    print("{} logs".format(doc_count))
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = logs_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    path = {"method": "GET", "path": "/status"}
+    path_count = logs_collection.count_documents(path)
+    print("{} status check".format(path_count))
