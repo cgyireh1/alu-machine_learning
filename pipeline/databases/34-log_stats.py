1742dc6827023055f8ff132c0bcce39ee760f021
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-11-02 21:14:04 +0200

    Update

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -8,7 +8,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017')
+    client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
     doc_count = logs_collection.count_documents({})
     print("{} logs".format(doc_count))
