269ea3eaf988cab55cb4218e3211275b064e4dd3
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-11-02 19:22:12 +0200

    Update 12-rating_genres

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,9 +1,7 @@
--- script that lists all genres in the database  by their rating
+-- script that lists all genres in the database by their rating
 SELECT tv_genres.name AS name, SUM(rate) AS rating_sum
 FROM tv_genres
-JOIN tv_show_genres
-    ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_show_ratings
-    ON tv_show_ratings.show_id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
 GROUP BY name
 ORDER BY rating_sum DESC;
