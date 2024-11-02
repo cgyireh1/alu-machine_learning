8cc718f65e7750a368fcbf96b4e42e04ee30ad4f
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2024-11-02 18:33:06 +0200

    Update 20-average_score

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,12 +1,12 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT)
+    IN user_id_new INT)
 BEGIN
     UPDATE users SET average_score=(
         SELECT AVG(score)
         FROM corrections
-        WHERE user_id = user_id)
-        WHERE id=user_id;
+        WHERE user_id = user_id_new)
+        WHERE id=user_id_new;
 END $$
-DELIMITER
+DELIMITER ;
