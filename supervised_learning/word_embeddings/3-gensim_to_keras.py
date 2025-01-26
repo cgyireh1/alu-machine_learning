076aee3674da3b72b159d14a9422dbf65d0181dc
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2025-01-26 11:41:46 +0200

    Update 3-gensim_to_keras

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -7,4 +7,4 @@ def gensim_to_keras(model):
     - model is a gensim word2vec models
     Returns: the trainable keras Embedding
     """
-    return model.wv.get_keras_embedding(train_embeddings=False)
+    return model.wv.get_keras_embedding(train_embeddings=True)
