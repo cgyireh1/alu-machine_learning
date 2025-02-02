78b040dcfb71a1e43d458f635e9b24221b3d1d89
gyirehcaroline <carolinegyireh801@gmail.com>
2025-02-02 10:50:25 +0200

    attention

--- supervised_learning/attention/11-transformer.py
+++ supervised_learning/attention/11-transformer.py
@@ -29,7 +29,7 @@ class Transformer(tf.keras.Model):
                                max_seq_input, drop_rate)
         self.decoder = Decoder(N, dm, h, hidden, target_vocab,
                                max_seq_target, drop_rate)
-        self.linear = tf.keras.layers.Dense(target_vocab)
+        self.linear = tf.keras.layers.Dense(target_vocab, kernel_initializer='he_normal')
 
     def call(self, inputs, target, training, encoder_mask,
              look_ahead_mask, decoder_mask):
@@ -48,5 +48,5 @@ class Transformer(tf.keras.Model):
         encoder_output = self.encoder(inputs, training, encoder_mask)
         decoder_output = self.decoder(target, encoder_output, training,
                                       look_ahead_mask, decoder_mask)
-        final_output = self.linear(decoder_output)
+        final_output = tf.nn.softmax(self.linear(decoder_output), axis=-1)  # Apply softmax
         return final_output
