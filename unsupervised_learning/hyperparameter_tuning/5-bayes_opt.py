488e2e9bdedef03c3e47e313ba83ab4b7d124400
gyirehcaroline <carolinegyireh801@gmail.com>
2025-03-02 22:21:19 +0200

    hyperparameter_tuning

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -17,7 +17,29 @@ class BayesianOptimization():
                  ac_samples, l=1, sigma_f=1, xsi=0.01,
                  minimize=True):
         """
-        * f is the black-box function to be optimized
+        f is the black-box function to be optimized
+        X_init is a numpy.ndarray of shape (t, 1) representing
+          the inputs already sampled with the black-box function
+        Y_init is a numpy.ndarray of shape (t, 1) representing
+          the outputs of the black-box function for each input
+          in X_init
+          - t is the number of initial samples
+        bounds is a tuple of (min, max) representing the bounds
+          of the space in which to look for the optimal point
+        ac_samples is the number of samples that should be analyzed
+          during acquisition
+        l is the length parameter for the kernel
+        sigma_f is the standard deviation given to the output of
+          the black-box function
+        xsi is the exploration-exploitation factor for acquisition
+        attributes:
+          - f: the black-box function
+          - gp: an instance of the class GaussianProcess
+          - X_s: a numpy.ndarray of shape (ac_samples, 1) containing
+            all acquisition sample points, evenly spaced between min
+            and max
+          - xsi: the exploration-exploitation factor
+          - minimize: a bool for minimization versus maximization
         """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
@@ -59,7 +81,7 @@ class BayesianOptimization():
 
     def optimize(self, iterations=100):
         """
-        Optimize method
+        The optimize method
         """
 
         X_all_s = []
