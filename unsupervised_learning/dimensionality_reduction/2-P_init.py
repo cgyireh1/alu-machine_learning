c2b7421355fdc147407c1107bc1693079311050e
gyirehcaroline <carolinegyireh801@gmail.com>
2025-03-02 20:09:09 +0200

    file update

--- unsupervised_learning/dimensionality_reduction/2-P_init.py
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -33,7 +33,8 @@ def P_init(X, perplexity):
     n = X.shape[0]
     mult = np.matmul(X, X.T)
     summation = np.sum(np.square(X), 1)
-    D = np.add(np.add(-2 * mult, summation), summation.T)
+    D = np.add(np.add(-2 * mult, summation).T, summation)
+    np.fill_diagonal(D, 0)
     P = np.zeros((n, n))
     betas = np.ones((n, 1))
     H = np.log2(perplexity)
